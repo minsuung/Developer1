06949f24ba3acf61cc0e8bb37743b59959c3ce17
김민성 <kimms6186@hanyang.ac.kr>
2026-01-13 22:23:01 +0900

    Initial Commit

---
+++ Developer1/Developer1App.swift
@@ -0,0 +1,17 @@
+//
+//  Developer1App.swift
+//  Developer1
+//
+//  Created by 김민성 on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Developer1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
